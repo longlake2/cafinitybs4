e6c2f93afcc51e5ba70b7e23cd76816480caf9d0
longlake2 <sites@emailhart.net>
2017-12-18 13:54:42 -0500

    styling update

--- archive-project.php
+++ archive-project.php
@@ -8,45 +8,55 @@
  */
 
 get_header(); ?>
- 
+
 	<div class="container">
 			<div id="primary" class="content-area-full">
-				<main id="main" class="site-main row" role="main">
- 
+				<main id="main" class="site-main" role="main">
+			
 				<?php
 				if ( have_posts() ) : ?>
- 
-					<header class="page-header col-md-12">
+					<div class="row justify-content-center text-center">
+						<header class="page-header col-md-6">
+
+							<?php $project_title = get_theme_mod( 'project_title', 'Projects' ); ?>
+
+							<?php if( $project_title != '') : ?>
+								<h1><?php echo $project_title; ?></h1>
+							<?php endif; ?>
+
+						</header><!-- .page-header -->
+					</div><!--  .row -->
+
+					<div class="row mt-5">
 						<?php
-							the_archive_title( '<h1 class="page-title">', '</h1>' );
-							the_archive_description( '<div class="archive-description">', '</div>' );
-						?>
-					</header><!-- .page-header -->
- 
-					<?php
-					/* Start the Loop */
-					while ( have_posts() ) : the_post();
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'template-parts/content', 'projects' );
-					endwhile; ?>
- 
-					<div class="col-md-12">
- 
-						<?php the_posts_navigation(); ?>
- 
-					</div><!--  .col-md-12 -->
- 
-				<?php else :
-					get_template_part( 'template-parts/content', 'none' );
-				endif; ?>
- 
+						/* Start the Loop */
+						while ( have_posts() ) : the_post();
+
+							/*
+							 * Include the Post-Format-specific template for the content.
+							 * If you want to override this in a child theme, then include a file
+							 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+							 */
+							get_template_part( 'template-parts/content', 'projects' );
+
+						endwhile; ?>
+
+
+						<div class="col-md-12">
+
+							<?php the_posts_navigation(); ?>
+
+						</div><!--  .col-md-12 -->
+
+					<?php else :
+
+						get_template_part( 'template-parts/content', 'none' );
+
+					endif; ?>
+				</div><!--  .row -->
 				</main><!-- #main -->
 			</div><!-- #primary -->
 	</div>
- 
+
 <?php
 get_footer();
--- inc/template-tags.php
+++ inc/template-tags.php
@@ -60,14 +60,6 @@ if ( ! function_exists( 'cafinitybs4_entry_footer' ) ) :
  * Prints HTML with meta information for the categories, tags and comments.
  */
 function cafinitybs4_entry_footer() {
-	// Hide category and tag text for pages.
-	if ( 'post' === get_post_type() ) {
-		/* translators: used between list items, there is a space after the comma */
-		$tags_list = get_the_tag_list( '', esc_html__( ', ', 'cafinitybs4' ) );
-		if ( $tags_list ) {
-			printf( '<span class="tags-links">' . esc_html__( '%1$s', 'cafinitybs4' ) . '</span>', $tags_list ); // WPCS: XSS OK.
-		}
-	}
 
 	edit_post_link(
 		sprintf(
@@ -80,7 +72,6 @@ function cafinitybs4_entry_footer() {
 	);
 }
 endif;
-
 /**
  * Returns true if a blog has more than 1 category.
  *
